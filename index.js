1fdc6e9bfc9a1a907b9aae20b0f84ec70660c852
zeke <zeke@sikelianos.com>
2014-05-30 15:34:44 -0400

    add support branches: user/repo#branch

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ module.exports = function(repo_url) {
 
   if (!repo_url) return null
 
-  var shorthand = repo_url.match(/^([\w-_]+)\/([\w-_\.]+)$/)
+  var shorthand = repo_url.match(/^([\w-_]+)\/([\w-_\.]+)#?([\w-_\.]+)?$/)
   var antiquated = repo_url.match(/^git@[\w-_\.]+:([\w-_]+)\/([\w-_\.]+)$/)
 
   if (shorthand) {
     obj.user = shorthand[1]
     obj.repo = shorthand[2]
+    obj.branch = shorthand[3] || "master"
   } else if (antiquated) {
     obj.user = antiquated[1]
     obj.repo = antiquated[2].replace(/\.git$/i, "")
+    obj.branch = "master"
   } else {
     if (!isUrl(repo_url)) return null
     var parsedURL = url.parse(repo_url)
@@ -26,6 +28,7 @@ module.exports = function(repo_url) {
     if (!parts) return null
     obj.user = parts[1]
     obj.repo = parts[2].replace(/\.git$/i, "")
+    obj.branch = "master"
   }
 
   obj.tarball_url = "https://api.github.com/repos/" + obj.user + "/" + obj.repo + "/tarball"
--- test/index.js
+++ test/index.js
@@ -4,87 +4,136 @@ var gh = require("../index")
 
 describe("github-url-to-object", function() {
 
+  describe("shorthand", function(){
+
+    it("supports user/repo style", function(){
+      var obj = gh("user/repo#branch")
+      assert.equal(obj.user, 'user')
+      assert.equal(obj.repo, 'repo')
+    })
+
+    it("supports user/repo#branch style", function(){
+      var obj = gh("user/repo#branch")
+      assert.equal(obj.user, 'user')
+      assert.equal(obj.repo, 'repo')
+      assert.equal(obj.branch, 'branch')
+    })
+
+    it("defaults to master branch", function(){
+      var obj = gh("user/repo")
+      assert.equal(obj.user, 'user')
+      assert.equal(obj.repo, 'repo')
+      assert.equal(obj.branch, 'master')
+    })
+
+  })
+
+  describe("oldschool", function(){
+
+    it("supports git@ URLs", function() {
+      var obj = gh("git@github.com:heroku/heroku-flags.git")
+      assert.equal(obj.user, 'heroku')
+      assert.equal(obj.repo, 'heroku-flags')
+    })
+
+    it("defaults to master branch", function() {
+      var obj = gh("git@github.com:heroku/heroku-flags.git")
+      assert.equal(obj.branch, 'master')
+    })
+
+    it("supports git:// URLs", function() {
+      var obj = gh("git://github.com/foo/bar.git")
+      assert.equal(obj.user, 'foo')
+      assert.equal(obj.repo, 'bar')
+    })
+
+    it("defaults to master branch", function() {
+      var obj = gh("git://github.com/foo/bar.git")
+      assert.equal(obj.branch, 'master')
+    })
+
+  })
+
+  describe("http", function(){
+
+    it("supports http URLs", function() {
+      var obj = gh("http://github.com/zeke/outlet.git")
+      assert.equal(obj.user, 'zeke')
+      assert.equal(obj.repo, 'outlet')
+    })
+
+    it("supports https URLs", function() {
+      var obj = gh("https://github.com/zeke/outlet.git")
+      assert.equal(obj.user, 'zeke')
+      assert.equal(obj.repo, 'outlet')
+    })
+
+    it("supports deep URLs", function() {
+      var obj = gh("https://github.com/zeke/ruby-rails-sample/blob/b1e1000fedb6ca448dd78702de4fc78dedfee48c/app.json")
+      assert.equal(obj.user, 'zeke')
+      assert.equal(obj.repo, 'ruby-rails-sample')
+    })
+
+    it("doesn't require .git extension", function() {
+      var obj = gh("https://github.com/zeke/outlet")
+      assert.equal(obj.user, 'zeke')
+      assert.equal(obj.repo, 'outlet')
+    })
+
+    it("defaults to master branch", function() {
+      var obj = gh("https://github.com/zeke/outlet")
+      assert.equal(obj.branch, 'master')
+    })
+
+  })
+
   describe("properties", function() {
     var obj
 
     before(function(){
-      obj = gh("git://github.com/foo-master/party-time.git")
+      obj = gh("zeke/ord")
     })
 
     it("user", function() {
-      assert.equal(obj.user, "foo-master")
+      assert.equal(obj.user, "zeke")
     })
 
     it("repo", function() {
-      assert.equal(obj.repo, "party-time")
+      assert.equal(obj.repo, "ord")
+    })
+
+    it("branch", function() {
+      assert.equal(obj.branch, "master")
     })
 
     it("tarball_url", function() {
-      assert.equal(obj.tarball_url, "https://api.github.com/repos/foo-master/party-time/tarball")
+      assert.equal(obj.tarball_url, "https://api.github.com/repos/zeke/ord/tarball")
     })
 
     it("https_url", function() {
-      assert.equal(obj.https_url, "https://github.com/foo-master/party-time")
+      assert.equal(obj.https_url, "https://github.com/zeke/ord")
     })
 
     it("travis_url", function() {
-      assert.equal(obj.travis_url, "https://travis-ci.org/foo-master/party-time")
+      assert.equal(obj.travis_url, "https://travis-ci.org/zeke/ord")
     })
 
   })
 
-  it("handles URLs without .git at the end", function() {
-    var obj = gh("https://github.com/zeke/outlet")
-    assert.equal(obj.user, 'zeke')
-    assert.equal(obj.repo, 'outlet')
-  })
+  describe("failure", function(){
 
-  it("handles http URLs", function() {
-    var obj = gh("http://github.com/zeke/outlet.git")
-    assert.equal(obj.user, 'zeke')
-    assert.equal(obj.repo, 'outlet')
-  })
+    it("returns null if url is falsy", function() {
+      assert.equal(gh(), null)
+      assert.equal(gh(null), null)
+      assert.equal(gh(undefined), null)
+      assert.equal(gh(""), null)
+    })
 
-  it("handles https URLs", function() {
-    var obj = gh("https://github.com/zeke/outlet.git")
-    assert.equal(obj.user, 'zeke')
-    assert.equal(obj.repo, 'outlet')
-  })
+    it("returns null for non-github URLs", function() {
+      var obj = gh("https://bitbucket.com/other/thing")
+      assert.equal(obj, null)
+    })
 
-  it("handles git:// URLs", function() {
-    var obj = gh("git://github.com/foo/bar.git")
-    assert.equal(obj.user, 'foo')
-    assert.equal(obj.repo, 'bar')
-  })
-
-  it("handles shorthand user/repo paths", function() {
-    var obj = gh("foo/bar")
-    assert.equal(obj.user, 'foo')
-    assert.equal(obj.repo, 'bar')
-  })
-
-  it("handles git@ URLs", function() {
-    var obj = gh("git@github.com:heroku/heroku-flags.git")
-    assert.equal(obj.user, 'heroku')
-    assert.equal(obj.repo, 'heroku-flags')
-  })
-
-  it("handles deep URLs", function() {
-    var obj = gh("https://github.com/zeke/ruby-rails-sample/blob/b1e1000fedb6ca448dd78702de4fc78dedfee48c/app.json")
-    assert.equal(obj.user, 'zeke')
-    assert.equal(obj.repo, 'ruby-rails-sample')
-  })
-
-  it("returns null if url is falsy", function() {
-    assert.equal(gh(), null)
-    assert.equal(gh(null), null)
-    assert.equal(gh(undefined), null)
-    assert.equal(gh(""), null)
-  })
-
-  it("returns null for non-github URLs", function() {
-    var obj = gh("https://bitbucket.com/other/thing")
-    assert.equal(obj, null)
   })
 
 })
