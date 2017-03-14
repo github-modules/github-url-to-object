5ddf5efa71c4737429d32b0aee59409075a3a88a
Federico Brigante <bfred-it@users.noreply.github.com>
2017-03-14 17:22:05 +0700

    Add built files

--- dist/commonjs.js
+++ dist/commonjs.js
@@ -1,4 +1,3 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.gh = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a){ return a(o,!0); }if(i){ return i(o,!0); }var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++){ s(r[o]); }return s})({1:[function(require,module,exports){
 'use strict'
 
 var url = require('url')
@@ -92,35 +91,3 @@ module.exports = function (repoUrl, opts) {
   return obj
 }
 
-},{"is-url":2,"url":3}],2:[function(require,module,exports){
-
-/**
- * Expose `isUrl`.
- */
-
-module.exports = isUrl;
-
-/**
- * Matcher.
- */
-
-var matcher = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/;
-
-/**
- * Loosely validate a URL `string`.
- *
- * @param {String} string
- * @return {Boolean}
- */
-
-function isUrl(string){
-  return matcher.test(string);
-}
-
-},{}],3:[function(require,module,exports){
-module.exports.parse = function (string) {
-  return new window.URL(string)
-}
-
-},{}]},{},[1])(1)
-});
